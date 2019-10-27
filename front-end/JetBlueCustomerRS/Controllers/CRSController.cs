6fd116f61fbb646aa5c5092036ce00ca37ec17b4
moyoonthego <39111972+moyoonthego@users.noreply.github.com>
2019-10-27 07:44:13 -0400

    done views (fake data)

--- front-end/JetBlueCustomerRS/Controllers/CRSController.cs
+++ front-end/JetBlueCustomerRS/Controllers/CRSController.cs
@@ -59,14 +59,13 @@ namespace JetBlueCRS.Controllers
         {
             List<DataPoint> dataPoints = new List<DataPoint>();
 
-            dataPoints.Add(new DataPoint("USA", 121));
-            dataPoints.Add(new DataPoint("Great Britain", 67));
-            dataPoints.Add(new DataPoint("China", 70));
-            dataPoints.Add(new DataPoint("Russia", 56));
-            dataPoints.Add(new DataPoint("Germany", -42));
-            dataPoints.Add(new DataPoint("Japan", 41));
-            dataPoints.Add(new DataPoint("France", 42));
-            dataPoints.Add(new DataPoint("South Korea", 21));
+            dataPoints.Add(new DataPoint("Chicken", 1.7));
+            dataPoints.Add(new DataPoint("Fish", -0.9));
+            dataPoints.Add(new DataPoint("Beef", 1));
+            dataPoints.Add(new DataPoint("Salad", 0.75));
+            dataPoints.Add(new DataPoint("Wine and Alcohol", -0.3));
+            dataPoints.Add(new DataPoint("Snack (Treats)", 2));
+            dataPoints.Add(new DataPoint("Snack (Chips)", -0.1));
 
             ViewBag.DataPoints = Newtonsoft.Json.JsonConvert.SerializeObject(dataPoints);
             return View();
@@ -77,14 +76,12 @@ namespace JetBlueCRS.Controllers
         {
             List<DataPoint> dataPoints = new List<DataPoint>();
 
-            dataPoints.Add(new DataPoint("USA", 121));
-            dataPoints.Add(new DataPoint("Great Britain", 67));
-            dataPoints.Add(new DataPoint("China", 70));
-            dataPoints.Add(new DataPoint("Russia", 56));
-            dataPoints.Add(new DataPoint("Germany", -42));
-            dataPoints.Add(new DataPoint("Japan", 41));
-            dataPoints.Add(new DataPoint("France", 42));
-            dataPoints.Add(new DataPoint("South Korea", 21));
+            dataPoints.Add(new DataPoint("-inf to -2", 8));
+            dataPoints.Add(new DataPoint("-2 to -1", 17));
+            dataPoints.Add(new DataPoint("-1 to 0", 67));
+            dataPoints.Add(new DataPoint("0 to 1", 70));
+            dataPoints.Add(new DataPoint("1 to 2", 94));
+            dataPoints.Add(new DataPoint("2 to inf", 6));
 
             ViewBag.DataPoints = Newtonsoft.Json.JsonConvert.SerializeObject(dataPoints);
             return View();
@@ -95,14 +92,12 @@ namespace JetBlueCRS.Controllers
         {
             List<DataPoint> dataPoints = new List<DataPoint>();
 
-            dataPoints.Add(new DataPoint("USA", 121));
-            dataPoints.Add(new DataPoint("Great Britain", 67));
-            dataPoints.Add(new DataPoint("China", 70));
-            dataPoints.Add(new DataPoint("Russia", 56));
-            dataPoints.Add(new DataPoint("Germany", -42));
-            dataPoints.Add(new DataPoint("Japan", 41));
-            dataPoints.Add(new DataPoint("France", 42));
-            dataPoints.Add(new DataPoint("South Korea", 21));
+            dataPoints.Add(new DataPoint("Service (General)", 25));
+            dataPoints.Add(new DataPoint("Customer Service", 10));
+            dataPoints.Add(new DataPoint("Management", 40));
+            dataPoints.Add(new DataPoint("Business", 20));
+            dataPoints.Add(new DataPoint("Bookings", 6));
+            dataPoints.Add(new DataPoint("Seatings", 10));
 
             ViewBag.DataPoints = Newtonsoft.Json.JsonConvert.SerializeObject(dataPoints);
             return View();
